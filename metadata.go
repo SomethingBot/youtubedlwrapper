e256e5bb18e1b62830203ae6e43b5f03db7c9cb3
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-09-14 17:16:03 -0500

    reorganize structs in metadata and rename some to non-plural versions for single entries

--- metadata.go
+++ metadata.go
@@ -1,81 +1,5 @@
 package youtubedlwrapper
 
-type Entries struct {
-	ID                 string             `json:"id"`
-	Title              string             `json:"title"`
-	Formats            []Formats          `json:"formats"`
-	Thumbnails         []Thumbnails       `json:"thumbnails"`
-	Description        string             `json:"description"`
-	UploadDate         string             `json:"upload_date"`
-	Uploader           string             `json:"uploader"`
-	UploaderID         string             `json:"uploader_id"`
-	UploaderURL        string             `json:"uploader_url"`
-	ChannelID          string             `json:"channel_id"`
-	ChannelURL         string             `json:"channel_url"`
-	Duration           int                `json:"duration"`
-	ViewCount          int                `json:"view_count"`
-	AverageRating      float64            `json:"average_rating"`
-	AgeLimit           int                `json:"age_limit"`
-	WebpageURL         string             `json:"webpage_url"`
-	Categories         []string           `json:"categories"`
-	Tags               []string           `json:"tags"`
-	IsLive             interface{}        `json:"is_live"`
-	LikeCount          int                `json:"like_count"`
-	DislikeCount       int                `json:"dislike_count"`
-	Channel            string             `json:"channel"`
-	Track              string             `json:"track"`
-	Artist             string             `json:"artist,omitempty"`
-	Album              string             `json:"album,omitempty"`
-	Creator            string             `json:"creator,omitempty"`
-	AltTitle           string             `json:"alt_title"`
-	Extractor          string             `json:"extractor"`
-	WebpageURLBasename string             `json:"webpage_url_basename"`
-	ExtractorKey       string             `json:"extractor_key"`
-	NEntries           int                `json:"n_entries"`
-	Playlist           string             `json:"playlist"`
-	PlaylistID         string             `json:"playlist_id"`
-	PlaylistTitle      string             `json:"playlist_title"`
-	PlaylistUploader   string             `json:"playlist_uploader"`
-	PlaylistUploaderID string             `json:"playlist_uploader_id"`
-	PlaylistIndex      int                `json:"playlist_index"`
-	Thumbnail          string             `json:"thumbnail"`
-	DisplayID          string             `json:"display_id"`
-	RequestedSubtitles interface{}        `json:"requested_subtitles"`
-	RequestedFormats   []RequestedFormats `json:"requested_formats"`
-	Format             string             `json:"format"`
-	FormatID           string             `json:"format_id"`
-	Width              int                `json:"width"`
-	Height             int                `json:"height"`
-	Resolution         interface{}        `json:"resolution"`
-	Fps                int                `json:"fps"`
-	Vcodec             string             `json:"vcodec"`
-	Vbr                float64            `json:"vbr"`
-	StretchedRatio     interface{}        `json:"stretched_ratio"`
-	Acodec             string             `json:"acodec"`
-	Abr                float64            `json:"abr"`
-	Ext                string             `json:"ext"`
-	AutomaticCaptions  map[string]string  `json:"automatic_captions,omitempty"`
-	Subtitles          map[string]string  `json:"subtitles,omitempty"`
-}
-
-type Playlist struct {
-	Type               string    `json:"_type"`
-	Entries            []Entries `json:"entries"`
-	ID                 string    `json:"id"`
-	Title              string    `json:"title"`
-	Uploader           string    `json:"uploader"`
-	UploaderID         string    `json:"uploader_id"`
-	UploaderURL        string    `json:"uploader_url"`
-	Extractor          string    `json:"extractor"`
-	WebpageURL         string    `json:"webpage_url"`
-	WebpageURLBasename string    `json:"webpage_url_basename"`
-	ExtractorKey       string    `json:"extractor_key"`
-}
-
-type DownloaderOptions struct {
-	HTTPChunkSize int `json:"http_chunk_size"`
-}
-
 type HTTPHeaders struct {
 	UserAgent      string `json:"User-Agent"`
 	AcceptCharset  string `json:"Accept-Charset"`
@@ -84,7 +8,11 @@ type HTTPHeaders struct {
 	AcceptLanguage string `json:"Accept-Language"`
 }
 
-type Formats struct {
+type DownloaderOptions struct {
+	HTTPChunkSize int `json:"http_chunk_size"`
+}
+
+type Format struct {
 	Asr               int               `json:"asr"`
 	Filesize          int               `json:"filesize"`
 	FormatID          string            `json:"format_id"`
@@ -107,15 +35,7 @@ type Formats struct {
 	Vbr               float64           `json:"vbr,omitempty"`
 }
 
-type Thumbnails struct {
-	Height     int    `json:"height"`
-	URL        string `json:"url"`
-	Width      int    `json:"width"`
-	Resolution string `json:"resolution"`
-	ID         string `json:"id"`
-}
-
-type RequestedFormats struct {
+type RequestedFormat struct {
 	Asr               interface{}       `json:"asr"`
 	Filesize          int               `json:"filesize"`
 	FormatID          string            `json:"format_id"`
@@ -136,4 +56,84 @@ type RequestedFormats struct {
 	Protocol          string            `json:"protocol"`
 	HTTPHeaders       HTTPHeaders       `json:"http_headers"`
 	Abr               float64           `json:"abr,omitempty"`
+}
+
+type Thumbnail struct {
+	Height     int    `json:"height"`
+	URL        string `json:"url"`
+	Width      int    `json:"width"`
+	Resolution string `json:"resolution"`
+	ID         string `json:"id"`
+}
+
+type Video struct {
+	ID                 string            `json:"id"`
+	Title              string            `json:"title"`
+	Formats            []Format          `json:"formats"`
+	Thumbnails         []Thumbnail       `json:"thumbnails"`
+	Description        string            `json:"description"`
+	UploadDate         string            `json:"upload_date"`
+	Uploader           string            `json:"uploader"`
+	UploaderID         string            `json:"uploader_id"`
+	UploaderURL        string            `json:"uploader_url"`
+	ChannelID          string            `json:"channel_id"`
+	ChannelURL         string            `json:"channel_url"`
+	Duration           int               `json:"duration"`
+	ViewCount          int               `json:"view_count"`
+	AverageRating      float64           `json:"average_rating"`
+	AgeLimit           int               `json:"age_limit"`
+	WebpageURL         string            `json:"webpage_url"`
+	Categories         []string          `json:"categories"`
+	Tags               []string          `json:"tags"`
+	IsLive             interface{}       `json:"is_live"`
+	LikeCount          int               `json:"like_count"`
+	DislikeCount       int               `json:"dislike_count"`
+	Channel            string            `json:"channel"`
+	Track              string            `json:"track"`
+	Artist             string            `json:"artist,omitempty"`
+	Album              string            `json:"album,omitempty"`
+	Creator            string            `json:"creator,omitempty"`
+	AltTitle           string            `json:"alt_title"`
+	Extractor          string            `json:"extractor"`
+	WebpageURLBasename string            `json:"webpage_url_basename"`
+	ExtractorKey       string            `json:"extractor_key"`
+	NEntries           int               `json:"n_entries"`
+	Playlist           string            `json:"playlist"`
+	PlaylistID         string            `json:"playlist_id"`
+	PlaylistTitle      string            `json:"playlist_title"`
+	PlaylistUploader   string            `json:"playlist_uploader"`
+	PlaylistUploaderID string            `json:"playlist_uploader_id"`
+	PlaylistIndex      int               `json:"playlist_index"`
+	Thumbnail          string            `json:"thumbnail"`
+	DisplayID          string            `json:"display_id"`
+	RequestedSubtitles interface{}       `json:"requested_subtitles"`
+	RequestedFormats   []RequestedFormat `json:"requested_formats"`
+	Format             string            `json:"format"`
+	FormatID           string            `json:"format_id"`
+	Width              int               `json:"width"`
+	Height             int               `json:"height"`
+	Resolution         interface{}       `json:"resolution"`
+	Fps                int               `json:"fps"`
+	Vcodec             string            `json:"vcodec"`
+	Vbr                float64           `json:"vbr"`
+	StretchedRatio     interface{}       `json:"stretched_ratio"`
+	Acodec             string            `json:"acodec"`
+	Abr                float64           `json:"abr"`
+	Ext                string            `json:"ext"`
+	AutomaticCaptions  map[string]string `json:"automatic_captions,omitempty"`
+	Subtitles          map[string]string `json:"subtitles,omitempty"`
+}
+
+type Playlist struct {
+	Type               string  `json:"_type"`
+	Videos             []Video `json:"entries"`
+	ID                 string  `json:"id"`
+	Title              string  `json:"title"`
+	Uploader           string  `json:"uploader"`
+	UploaderID         string  `json:"uploader_id"`
+	UploaderURL        string  `json:"uploader_url"`
+	Extractor          string  `json:"extractor"`
+	WebpageURL         string  `json:"webpage_url"`
+	WebpageURLBasename string  `json:"webpage_url_basename"`
+	ExtractorKey       string  `json:"extractor_key"`
 }
