14d65b20160df89a85fbb7411b5f40fd58545a13
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-08 19:03:58 -0500

    change init to parse a flag for testdata and separate logic for loading playlist/videos

--- wrapper_test.go
+++ wrapper_test.go
@@ -2,6 +2,7 @@ package youtubedlwrapper
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -15,32 +16,36 @@ type testVideo struct {
 	videoMetadata VideoMetadata
 }
 
+type testPlaylist struct {
+	data             json.RawMessage
+	playlistMetadata PlaylistMetadata
+}
+
 var testVideos []testVideo
+var testPlaylists []testPlaylist
 
-func init() {
-	testDataDir := "./testdata/videometadata/"
-
+func parseTestVideos(testDataDir string) error {
 	files, err := os.ReadDir(testDataDir)
 	if err != nil {
-		panic(fmt.Errorf("could not readdir (%v) error (%v)\n", testDataDir, err))
+		return fmt.Errorf("could not readdir (%v) error (%v)\n", testDataDir, err)
 	}
 
 	var osFile *os.File
 	for _, file := range files {
 		osFile, err = os.Open(testDataDir + file.Name())
 		if err != nil {
-			panic(fmt.Errorf("could not open file (%v), error (%v)\n", file.Name(), err))
+			return fmt.Errorf("could not open file (%v), error (%v)\n", file.Name(), err)
 		}
 
 		testVideo := testVideo{}
 		testVideo.data, err = io.ReadAll(osFile)
 		if err != nil {
-			panic(fmt.Errorf("could not read content from file (%v), error (%v)\n", osFile.Name(), err))
+			return fmt.Errorf("could not read content from file (%v), error (%v)\n", osFile.Name(), err)
 		}
 
 		err = osFile.Close()
 		if err != nil {
-			panic(fmt.Errorf("could not close file (%v), error (%v)\n", osFile.Name(), err))
+			return fmt.Errorf("could not close file (%v), error (%v)\n", osFile.Name(), err)
 		}
 
 		decoder := json.NewDecoder(strings.NewReader(string(testVideo.data)))
@@ -48,12 +53,69 @@ func init() {
 
 		err = decoder.Decode(&testVideo.videoMetadata)
 		if err != nil {
-			panic(fmt.Errorf("file (%v), could not unmarshal testVideo.data into testVideo.VideoMetadata, error (%v)\n", osFile.Name(), err))
+			return fmt.Errorf("file (%v), could not unmarshal testVideo.data into testVideo.VideoMetadata, error (%v)\n", osFile.Name(), err)
 		}
 
 		testVideos = append(testVideos, testVideo)
 		//todo: check test.videoMetadata for completeness
 	}
+
+	return nil
+}
+
+func parseTestPlaylists(testDataDir string) error {
+	files, err := os.ReadDir(testDataDir)
+	if err != nil {
+		return fmt.Errorf("could not readdir (%v) error (%v)\n", testDataDir, err)
+	}
+
+	var osFile *os.File
+	for _, file := range files {
+		osFile, err = os.Open(testDataDir + file.Name())
+		if err != nil {
+			return fmt.Errorf("could not open file (%v), error (%v)\n", file.Name(), err)
+		}
+
+		testPlaylist := testPlaylist{}
+		testPlaylist.data, err = io.ReadAll(osFile)
+		if err != nil {
+			return fmt.Errorf("could not read content from file (%v), error (%v)\n", osFile.Name(), err)
+		}
+
+		err = osFile.Close()
+		if err != nil {
+			return fmt.Errorf("could not close file (%v), error (%v)\n", osFile.Name(), err)
+		}
+
+		decoder := json.NewDecoder(strings.NewReader(string(testPlaylist.data)))
+		decoder.DisallowUnknownFields()
+
+		err = decoder.Decode(&testPlaylist.playlistMetadata)
+		if err != nil {
+			return fmt.Errorf("file (%v), could not unmarshal testVideo.data into testVideo.VideoMetadata, error (%v)\n", osFile.Name(), err)
+		}
+
+		testPlaylists = append(testPlaylists, testPlaylist)
+		//todo: check test.videoMetadata for completeness
+	}
+	return nil
+}
+
+func init() {
+	testDataDir := flag.String("testdatadir", "./testdata/", "directory to use for testdata, needs subdirs videometadata and playlistmetadata")
+	err := parseTestVideos(*testDataDir + "videometadata/")
+	if err != nil {
+		panic(err)
+	}
+	err = parseTestPlaylists(*testDataDir + "playlistmetadata/")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	os.Exit(m.Run())
 }
 
 func TestYoutubeDLWrapper_GetMetaData(t *testing.T) {
@@ -92,3 +154,12 @@ func TestYoutubeDLWrapper_GetMetaData(t *testing.T) {
 
 	t.Error("Did not receive an error from bad url passed to youtube-dl")
 }
+
+//func TestWrapper_GetPlaylistMetadata(t *testing.T) {
+//	t.Parallel()
+//	cmdMocker := commandMocker{
+//		stdinData:  "",
+//		stdoutData: string(testVideos[0].data),
+//		stderrData: "",
+//	}
+//}
